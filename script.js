e5a4678df89b9ba4ee7ff021b9492ae506ed140b
ravindusr <ravindurashmika121@gmail.com>
2024-07-16 12:24:03 -0700

    added functions

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ const loginForm = document.getElementById('login-form');
             signupForm.classList.add('hidden');
             loginForm.classList.remove('hidden');
         });
+
+    
+
+function togglePasswordVisibility(passwordInputId, toggleButtonId, iconId) {
+        const passwordInput = document.getElementById(passwordInputId);
+        const toggleButton = document.getElementById(toggleButtonId);
+        const icon = document.getElementById(iconId);
+
+        toggleButton.addEventListener('click', () => {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                icon.classList.remove('bi-eye');
+                icon.classList.add('eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                icon.classList.remove('bi-eye-slash');
+                icon.classList.add('bi-eye');
+            }
+        });
+    }
+
+    togglePasswordVisibility('login-password', 'login-password-toggle', 'login-password-icon');
+
+    togglePasswordVisibility('signup-password', 'signup-password-toggle', 'signup-password-icon');
+
